70b966bcfd5f0ec94854cfcafd6b0c07768afa54
AubBantolo <jhuliet14@gmail.com>
2023-08-25 20:23:36 +0800

    feat: created companyResponse dto

--- src/main/java/com/afs/restapi/service/mapper/CompanyResponse.java
+++ src/main/java/com/afs/restapi/service/mapper/CompanyResponse.java
@@ -1,2 +1,36 @@
-package com.afs.restapi.service.mapper;public class CompanyResponse {
+package com.afs.restapi.service.mapper;
+
+public class CompanyResponse {
+
+    private Long id;
+
+    private String name;
+
+    private Integer employeesCount;
+    public CompanyResponse(){
+
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getEmployeesCount() {
+        return employeesCount;
+    }
+
+    public void setEmployeesCount(Integer employeesCount) {
+        this.employeesCount = employeesCount;
+    }
 }
