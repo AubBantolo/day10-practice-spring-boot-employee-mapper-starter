6cadf94983ec23b347757b1a2f47249e7f03a36e
AubBantolo <jhuliet14@gmail.com>
2023-08-25 20:24:41 +0800

    feat: created company mapper with toEntity and toResponse

--- src/main/java/com/afs/restapi/service/mapper/CompanyMapper.java
+++ src/main/java/com/afs/restapi/service/mapper/CompanyMapper.java
@@ -1,2 +1,22 @@
-package com.afs.restapi.service.mapper;public class CompanyMapper {
+package com.afs.restapi.service.mapper;
+
+import com.afs.restapi.entity.Company;
+import com.afs.restapi.service.dto.CompanyRequest;
+import org.springframework.beans.BeanUtils;
+
+public class CompanyMapper {
+    public static Company toEntity(CompanyRequest companyRequest) {
+        Company company = new Company();
+        BeanUtils.copyProperties(companyRequest, company);
+        return company;
+    }
+
+    public static CompanyResponse toResponse(Company company) {
+        CompanyResponse companyResponse = new CompanyResponse();
+        BeanUtils.copyProperties(company, companyResponse);
+
+        var employees = company.getEmployees();
+        companyResponse.setEmployeesCount((employees == null) ? 0 : employees.size());
+        return companyResponse;
+    }
 }
