6a1c545993fc99e1fbcb84c6e6836cc79dec55cc
AubBantolo <jhuliet14@gmail.com>
2023-08-25 20:23:14 +0800

    feat: created companyRequest dto

--- src/main/java/com/afs/restapi/service/dto/CompanyRequest.java
+++ src/main/java/com/afs/restapi/service/dto/CompanyRequest.java
@@ -1,2 +1,37 @@
-package com.afs.restapi.service.dto;public class CompanyRequest {
+package com.afs.restapi.service.dto;
+
+import com.afs.restapi.entity.Employee;
+
+import java.util.List;
+
+public class CompanyRequest {
+
+    private String name;
+
+    private List<Employee> employees;
+
+    public CompanyRequest(){
+
+    }
+
+    public CompanyRequest(String name, List<Employee> employees) {
+        this.name = name;
+        this.employees = employees;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
 }
